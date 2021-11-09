4050b9f740bd46768b45fd15572887e62935b2d7
Gerhard Popp <gerhrad@poppgerhard.at>
2021-11-09 10:55:06 +0100

    add project header incl lightbox and slider

--- assets/js/components/ImageCarousel.vue
+++ assets/js/components/ImageCarousel.vue
@@ -17,8 +17,10 @@ export default {
       var slides = [];
       this.images.map((url, index) => {
 
-        var slide = '<div class="slider" @click="$root.$emit(\'openlightbox\', { index : ' + index + ' })">' +
+        var slide = '<div class="slider relative" @click="$root.$emit(\'openlightbox\', { index : ' + index + ' })">' +
             '<img src="' + url.url + '" class="w-full">' +
+            '<div class="absolute top-0 left-0 w-full h-full flex justify-center items-center"><div class="p-5 rounded-full bg-golden bg-opacity-25 cursor-pointer">' +
+            '<svg class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg></div></div> ' +
             '</div>';
         slides.push(slide);
       })
