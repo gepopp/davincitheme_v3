a8a075b0cb2bc2a420d2a425a9d645a2d01654bf
Gerhard Popp <gerhrad@poppgerhard.at>
2021-11-10 17:00:25 +0100

    add all project canvases

--- templates/headers/open.php
+++ templates/headers/open.php
@@ -1,5 +1,5 @@
 <div class="z-50 <?php is_singular('project') ? 'p-0' : 'py-5' ?> mb-10 border-b-2 border-golden shadow-xl"
      style="
 	     background-image: url(<?php echo get_field('field_5f207d5615988', 'option') ?>);
-	     background-size: auto 100%"
+	     background-size: cover"
 >
