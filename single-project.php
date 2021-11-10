69a8ff7278316599583b69053c5a4f99c37ae506
Gerhard Popp <gerhrad@poppgerhard.at>
2021-11-10 16:33:15 +0100

    add all project canvases

--- single-project.php
+++ single-project.php
@@ -42,14 +42,14 @@ endif;
 	<?php get_template_part( 'headers', 'banderole' ); ?>
     <section class="container mx-auto px-5">
         <div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-10">
-            <div class="lg:my-24 mb-10 mt-10">
+            <div class="lg:my-12 my-10">
                 <h3 class="text-base font-bold mb-0 leading-none text-white">
 					<?php the_field( 'field_5f216411f6f5a' ) ?></h3>
                 <h1 class="text-3xl text-golden leading-none mb-8 break-words"><?php the_title() ?></h1>
                 <div class="text-white text-sm"><?php the_field( 'field_5f21641ef6f5b', null, false ) ?></p>
                 </div>
             </div>
-            <div class="lg:my-24 mb-10">
+            <div class="lg:my-12 my-10">
 				<?php if ( ! empty( $gallery ) ): ?>
                     <image-carousel :images="<?php echo htmlspecialchars( json_encode( $urls, ENT_QUOTES ) ) ?>"></image-carousel>
                     <image-light-box :media="<?php echo htmlspecialchars( json_encode( $lightbox, ENT_QUOTES ) ) ?>"></image-light-box>
