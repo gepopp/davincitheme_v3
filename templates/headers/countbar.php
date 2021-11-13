4f8bfdc95495f6d80170795c4accb7bd23d2ed91
Gerhard Popp <gerhrad@poppgerhard.at>
2021-11-13 15:13:17 +0100

    linnkjuice

--- templates/headers/countbar.php
+++ templates/headers/countbar.php
@@ -4,30 +4,30 @@
 
             <div>
                 <div class="flex flex-col justify-center items-center p-10">
-                    <countup :end-val="<?php echo wp_count_posts('project')->publish ?>" :delay="1000"></countup>
+                    <countup :end-val="<?php echo wp_count_posts( 'project' )->publish ?>" :delay="1000"></countup>
                     <p class="text-lg font-semibold">Projekte online</p>
-                    <a href="<?php echo home_url('referenzen') ?>" class="block bg-darkblue text-white text-center shadow-lg px-5 py-3 mt-3">Zu den Projekten</a>
+                    <a href="<?php echo home_url( 'referenzen' ) ?>" class="block bg-darkblue text-white text-center shadow-lg px-5 py-3 mt-3">Zu den Projekten</a>
 
                 </div>
             </div>
 
-
-            <div class="flex flex-col justify-center items-center p-10">
-                <countup :end-val="<?php echo wp_count_posts('immobilie')->publish ?>" :delay="2000"></countup>
-                <p class="text-lg font-semibold">Wohneinheiten online</p>
-                <a href="<?php echo get_post_type_archive_link('immobilie') ?>" class="block bg-darkblue text-white text-center shadow-lg px-5 py-3 mt-3">Zu den Immobilien</a>
+            <div>
+                <div class="flex flex-col justify-center items-center p-10">
+                    <countup :end-val="<?php echo wp_count_posts( 'immobilie' )->publish ?>" :delay="2000"></countup>
+                    <p class="text-lg font-semibold">Wohneinheiten online</p>
+                    <a href="<?php echo get_post_type_archive_link( 'immobilie' ) ?>" class="block bg-darkblue text-white text-center shadow-lg px-5 py-3 mt-3">Zu den Immobilien</a>
+                </div>
             </div>
 
 
             <div>
                 <div class="flex flex-col justify-center items-center p-10">
-                    <countup :end-val="<?php echo wp_count_posts('team_member')->publish ?>" :delay="3000"></countup>
+                    <countup :end-val="<?php echo wp_count_posts( 'team_member' )->publish ?>" :delay="3000"></countup>
                     <p class="text-lg font-semibold">Teammitglieder</p>
-                    <a href="<?php echo get_post_type_archive_link('team_member') ?>" class="block bg-darkblue text-white text-center shadow-lg px-5 py-3 mt-3">Zum Team</a>
+                    <a href="<?php echo get_post_type_archive_link( 'team_member' ) ?>" class="block bg-darkblue text-white text-center shadow-lg px-5 py-3 mt-3">Zum Team</a>
 
                 </div>
             </div>
-
 
 
         </div>
