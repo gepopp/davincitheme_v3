ae76eba1b3d24e9284603e8f6c73beb7570574a4
Gerhard Popp <gerhrad@poppgerhard.at>
2021-12-10 09:32:33 +0100

    singel team member

--- single-team_member.php
+++ single-team_member.php
@@ -24,27 +24,41 @@ get_template_part( 'headers', 'banderole' ); ?>
 				<?php the_post_thumbnail( '16by9' ); ?>
             </div>
     </section>
+
+<?php
+
+$vcf = get_field( 'field_60efd6ac8f6e3' );
+$phone = empty(get_field('field_61b07c6bfb2ff')) ? get_field('field_5f23fa86c024f') : get_field('field_61b07c6bfb2ff');
+
+?>
+
+
     <div class="bg-darkblue border-t border-golden">
-        <div class="container mx-auto">
-            <div class="grid grid-cols-1 sm:grid-cols-3 text-white py-3">
-                <a href="" class="flex justify-between w-1/2 items-center py-3">
-                    <div class="bg-lightblue rounded-full p-2 mr-3 inline-block">
-                        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
-                        </svg>
-                    </div>
-                    <span class="text-lg">Kontakt speichern</span>
-                </a>
-                <a href="" class="flex justify-between w-1/2 items-center py-3">
-                    <div class="bg-lightblue rounded-full p-2 mr-3 inline-block">
+        <div class="container mx-auto px-5">
+            <div class="grid grid-cols-1 <?php echo empty( $vcf ) ? 'sm:grid-cols-2' : 'sm:grid-cols-3' ?> text-white py-3">
+
+				<?php if ( ! empty( $vcf ) ): ?>
+                    <a href="<?php echo $vcf ?>" class="flex lg:justify-center items-center py-3">
+                        <div class="bg-lightblue rounded-full p-2 mr-5 inline-block">
+                            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
+                            </svg>
+                        </div>
+                        <span class="text-lg">Kontakt speichern</span>
+                    </a>
+				<?php endif; ?>
+
+
+                <a href="tel:<?php echo $phone ?>" class="flex lg:justify-center items-center py-3">
+                    <div class="bg-lightblue rounded-full p-2 mr-5 inline-block">
                         <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z"></path>
                         </svg>
                     </div>
                     <span class="text-lg">Kontakt anrufen</span>
                 </a>
-                <a href="" class="flex justify-between w-1/2 items-center py-3">
-                    <div class="bg-lightblue rounded-full p-2 mr-3 inline-block">
+                <a href="mailto:<?php the_field('field_5f23fa7ec024e'); ?>" class="flex lg:justify-center items-center py-3">
+                    <div class="bg-lightblue rounded-full p-2 mr-5 inline-block">
                         <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"></path>
                         </svg>
